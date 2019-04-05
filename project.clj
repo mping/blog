10f6fa55d8afa5f4590ff7a9642345e668e7b8ca
mcorbin <corbin.math@gmail.com>
2019-04-05 21:33:43 +0200

    ebpf english

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://mcorbin.fr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-devel "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring/ring-devel "1.7.1"]
                  [compojure "1.6.1"]
                  [ring-server "0.5.0"]
                  [cryogen-asciidoc "0.1.4"]
                  [cryogen-core "0.1.65-mcorbin"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.12.5"]]
   :main cryogen.core
   :ring {:init cryogen.server/init
          :handler cryogen.server/handler})
